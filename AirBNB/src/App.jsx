6a07a3c61a9a34ede85c572b2065c47f51f1a217
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-15 09:15:43 +0500

    comments for a challenge

--- AirBNB/src/App.jsx
+++ AirBNB/src/App.jsx
@@ -8,34 +8,7 @@ import Card from './Components/Card';
 import data from './data';
 
 function App() {
-/*
-Challenge:
 
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-
-		<Card
-				img='/src/Components/images/katie-zaferes.png'
-				rating='5.0'
-				reviewCount={6}
-				country='Whatever you want'
-				title='Life Lessons with Katie Zaferes'
-				price={136}
-			></Card>
-			img={item.coverImg}
-	rating={item.stats.rating}
-	reviewCount={item.stats.reviewCount}
-	country={item.country}
-	location={item.location}
-	title={item.title}
-	price={item.price}
-	openSpots={item.openSpots}
-*/
 const dataElement = data.map(item => {
 	return <Card 
 	key={item.id}
